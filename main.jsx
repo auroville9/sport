2fbb9f5e3a0119789f810086d25d3bbad154b6e5
auroville9 <garciajordan92400@gmail.com>
2025-06-25 11:15:22 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import SuiviSport from './SuiviSport'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SuiviSport />
+  </React.StrictMode>
+)
